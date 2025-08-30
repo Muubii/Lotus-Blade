6dd8fc65ef03795323c635394b090be277b40150
Sam Siu <117384741+Muubii@users.noreply.github.com>
2025-08-30 11:08:45 +0200

    player movement

--- Scene/character_body_2d.gd
+++ Scene/character_body_2d.gd
@@ -1,11 +1,24 @@
 extends CharacterBody2D
 
 @export var speed = 400
+@onready var animated_sprite = $AnimatedSprite2D
 
 func get_input():
-	var input_direction = Input.get_vector("left","right", "up","down")
+	# Read four directions at once
+	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
-	
+
+	# Direction-based animation logic
+	if input_direction.y < 0:
+		animated_sprite.play("run_up")
+	elif input_direction.y > 0:
+		animated_sprite.play("run_down")
+	elif input_direction.x != 0:
+		animated_sprite.play("run_right")
+		animated_sprite.flip_h = input_direction.x < 0
+	else:
+		animated_sprite.play("idle_down")
+
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
